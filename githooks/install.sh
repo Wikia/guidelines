a41cd472fb4dc1c041032b180b6adbd9a01b8cfd
Scott Rabin <srabin@wikia-inc.com>
2014-05-29 20:00:12 +0000

    Add a convenience installer for githooks
    Running `sh /path/to/this/file` from the root of a git repository will
    correctly install the hooks.

---
+++ githooks/install.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+PRE_COMMIT=.git/hooks/pre-commit
+DIR=$(dirname $0)
+
+if [ ! -d .git ]; then
+	echo "Git hooks install script must be run from the root directory of a git repository" 1>&2
+	exit 1
+fi
+
+if [ -e "$PRE_COMMIT" ]; then
+	rm "$PRE_COMMIT"
+fi
+
+ln -s "$DIR/pre-commit" "$PRE_COMMIT"
