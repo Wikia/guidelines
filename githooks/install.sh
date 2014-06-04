5b72b634f93703764def289b3d60838779d29dfc
Scott Rabin <srabin@wikia-inc.com>
2014-06-04 20:25:14 +0000

    whitespace

--- githooks/install.sh
+++ githooks/install.sh
@@ -4,8 +4,8 @@ PRE_COMMIT=.git/hooks/pre-commit
 DIR=$(cd -- "$(dirname "$0")"; pwd -P)
 
 if [[ ! -d .git ]]; then
-	echo "Git hooks install script must be run from the root directory of a git repository" 1>&2
-	exit 1
+  echo "Git hooks install script must be run from the root directory of a git repository" 1>&2
+  exit 1
 fi
 
 if [[ -e $PRE_COMMIT || -h $PRE_COMMIT ]]; then
