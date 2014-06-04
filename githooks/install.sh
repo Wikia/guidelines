9f1bb2682da7301fd14a1e79268d0dd4ab331f07
Jonathan Robson <jnrbsn@gmail.com>
2014-06-04 12:59:06 -0700

    Use [[ ]] instead of [ ] in bash scripts.

--- githooks/install.sh
+++ githooks/install.sh
@@ -3,12 +3,12 @@
 PRE_COMMIT=.git/hooks/pre-commit
 DIR=$(dirname $0)
 
-if [ ! -d .git ]; then
+if [[ ! -d .git ]]; then
 	echo "Git hooks install script must be run from the root directory of a git repository" 1>&2
 	exit 1
 fi
 
-if [ -e "$PRE_COMMIT" ]; then
+if [[ -e $PRE_COMMIT ]]; then
 	rm "$PRE_COMMIT"
 fi
 
